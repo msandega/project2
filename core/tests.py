e8d12c9c34483650d334534ba47c91360a34d80d
msandega <ingato.andega@gmail.com>
2025-12-28 16:55:50 +0300

    Refactor test structure by defining SampleTestCase class and moving addition test into it

--- core/tests.py
+++ core/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
 
-# Create your tests here.
-def test_example():
-    assert 1 + 1 == 2
+class SampleTestCase(TestCase):
+    def test_addition(self):
+        assert 1 + 1 == 2
